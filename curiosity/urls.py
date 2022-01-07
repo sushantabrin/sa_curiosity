03c7c08557626e18d605f1c5f24e6450fa12748a
Sushant Abrin <97043022+sushantabrin@users.noreply.github.com>
2022-01-08 03:11:22 +0530

    Welcome Updated

--- curiosity/urls.py
+++ curiosity/urls.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import home, post, category
+from .views import home, post, category, index
 # from django.conf.urls import url
 
 urlpatterns = [
     path('home/',home),
+    path('index/',index),
     path('curiosity/<slug:url>', post),
     path('category/<slug:url>',category)
 ]
